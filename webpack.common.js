01dc80223d506fe68fadfb22281244872a57a184
Mohammed Ismail Ansari <team.terminal@gmail.com>
2018-04-23 19:12:53 +0000

    replace jshint and jscs with eslint

--- webpack.common.js
+++ webpack.common.js
@@ -54,21 +54,11 @@ module.exports = {
             },
             {
                 test: /\.(jsx|js)$/,
-                enforce: "pre",
+                enforce: 'pre',
                 exclude: /node_modules/,
                 use: [
                     {
-                        loader: "jsxhint-loader"
-                    }
-                ]
-            },
-            {
-                test: /\.(jsx|js)$/,
-                enforce: "pre",
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: "jscs-loader"
+                        loader: 'eslint-loader'
                     }
                 ]
             },
